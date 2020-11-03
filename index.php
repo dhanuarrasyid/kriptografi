244a259feb1c381d70c8f77ce9dfd97ab52cf355
Firandhanu A. R <60869225+dhanuarrasyid@users.noreply.github.com>
2020-11-03 19:39:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<html>
+<head><title>FORM UNTUK ENKRIPSI</title></head><body><form action="enkripsi.php" method="get">Plainteks : <input type="text" name="kata">
+<br>Key : <input type="text" name="key" maxlength="5">
+<br><input type="submit" value="kirim"><input type="reset" value="ulangi"></form></body></html>
